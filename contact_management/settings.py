7f085fd3c966074f4d738885ba0324d2537854ba
Zunaid Hassan <67002737+zunaid-hassan@users.noreply.github.com>
2024-09-17 22:47:28 +0600

    update

--- contact_management/settings.py
+++ contact_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-p)ikl*@gui#e$^^tw%#u(2v3d-7rcj^iyk@wpig(hvsohfg*o0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
