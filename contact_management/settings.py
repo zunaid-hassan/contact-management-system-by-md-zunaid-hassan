64463b7de563fda8dc2ba43be70a77105aaccc27
Zunaid Hassan <67002737+zunaid-hassan@users.noreply.github.com>
2024-09-17 22:48:23 +0600

    update

--- contact_management/settings.py
+++ contact_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-p)ikl*@gui#e$^^tw%#u(2v3d-7rcj^iyk@wpig(hvsohfg*o0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
